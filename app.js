9ff07e6f2cb523a124331bd80b111b6308b77c13
Shahesthan-Anjum <101544115+Shahesthan-Anjum@users.noreply.github.com>
2023-10-27 19:58:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,59 @@
-const mongoose=require('mongoose');
-//require('dotenv').config()
-const dotenv = require("dotenv");
-require('dotenv').config()
-//dotenv.config();
+const express=require('express');
+const app=express();
+const dbConnect=require('./db/dbConnect')
+const User=require('./db/userModel')
+dbConnect()
+const newUser=new User({
+    email:'anjum@example.com',
+    password:'anjum123'
+})
+/*newUser.save()
+.then(()=>{
+    console.log('user inserted successfully')
+})
+.catch((error)=>{
+    console.error('Error inserting user',error)
+})
+User.find()
+.then((users)=>{
+    console.log('Users found',users)
+})
+.catch((error)=>{
+    console.error('Error Finding users',error)
+})*/
+//User.updateMany
+/* User.updateOne({email:'anjum@example.com'},{password:'newanjum123'})
+.then(()=>{
+    console.log('USer updated successfully')
+})
+.catch((error)=>{
+    console.error('Error updating users',error)
+})
+User.deleteOne({email:'anjum@example.com'},{password:'newanjum123'})
+.then(()=>{
+    console.log('USer deleted successfully')
+})
+.catch((error)=>{
+    console.error('Error deleting users',error)
+}) */
+/* User.updateMany({email:'anjum@example.com'},{password:'newanjum123'})
+.then((result)=>{
+    console.log('USer updated successfully',result)
+})
+.catch((error)=>{
+    console.error('Error updating users',error)
+}) */
 
-async function dbConnect(){
-   
-    mongoose.connect(`${process.env.DB_URL}`,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(()=>{
-        console.log('successfully connected to mongodb ')
-    }).catch((error)=>{
-    console.log("unale to connect to mongodb")
-    console.error(error)
-    })
-}
-module.exports=dbConnect;
+User.collection.createIndex({email:1},{unique:true})
+User.find({email:'john@examplecom'}).select({email:1,_id:0})
+.then((users)=>{
+    console.log('users found',users)
+})
+.catch((error)=>{
+    console.error('error finding users',error)
+})
+
+
+
+
+module.exports=app;
